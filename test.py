ad5347407d857a0dd6cd628aab819639ca142ccf
Myeongseokkang <kms00129@naver.com>
2021-12-01 15:21:36 +0900

    fix: token creator 수 오류 수정 코드

--- test.py
+++ test.py
@@ -1,68 +1,15 @@
 import pymysql
 import pandas as pd
-from lib.Thegraph import *
-from lib.FeatureLib import *
 from tqdm import tqdm
-
-datas = pd.read_csv('./files/Pairs_v2.5.csv',encoding='utf-8-sig').to_dict('records')
-error_list = []
-
-for data in tqdm(datas,desc="processing rate"):
-    try:
-        # Feature Part 1
-        holders = get_holders(data['id'])
-        Lock_ratio = get_Lock_ratio(holders)
-        LP_avg, LP_stdev = calc_LP_distribution(holders)
-        LP_Creator_ratio = get_Creator_ratio(holders,data['creator_address'])
-        data['holders'] = holders
-        data['Lock_ratio'] = Lock_ratio
-        data['LP_avg'] = LP_avg
-        data['LP_stdev'] = LP_stdev
-        data['LP_Creator_ratio'] = LP_Creator_ratio
-        
-        # Feature Part 2
-        pair_address = data['id']
-        mint_data_transaction = call_theGraph_mint(pair_address)
-        swap_data_transaction = call_theGraph_swap(pair_address)
-        burn_data_transaction = call_theGraph_burn(pair_address)
-
-        mint_count = len(mint_data_transaction)
-        swap_count = len(swap_data_transaction)
-        burn_count = len(burn_data_transaction)
-
-        initial_timestamp = int(mint_data_transaction[0]['timestamp'])
-        last_timestamp = get_last_timestamp(mint_data_transaction,swap_data_transaction,burn_data_transaction)
-        active_period = last_timestamp - initial_timestamp
-        mint_mean_period = int(get_mint_mean_period(mint_data_transaction,initial_timestamp))
-        swap_mean_period = int(get_swap_mean_period(swap_data_transaction,initial_timestamp))
-        burn_mean_period = int(get_burn_mean_period(burn_data_transaction,initial_timestamp))
-
-        swapIn,swapOut = swap_IO_rate(swap_data_transaction,token_index(data))
-
-        data['mint_count'] = mint_count
-        data['swap_count'] = swap_count
-        data['burn_count'] = burn_count
-        
-        data['active_period'] = active_period
-        data['mint_mean_period'] = mint_mean_period
-        data['swap_mean_period'] = swap_mean_period
-        data['burn_mean_period'] = burn_mean_period
-
-        data['swapIn'] = swapIn
-        data['swapOut'] = swapOut
-
-        # Feature part 3
-        token_holders = get_holders(data['token00.id'])   
-        burn_ratio = get_burn_ratio(token_holders)
-        creator_ratio = get_creator_ratio(token_holders,data['creator_address'])
-
-        data['token_holders'] = token_holders
-        data['burn_ratio'] = burn_ratio
-        data['creator_ratio'] = creator_ratio
-
-    
-    except Exception as e:
-        data['error'] = e
-        print(e)
-
-pd.DataFrame(datas).to_csv('./drive/MyDrive/pairs_v2.6.csv',encoding='utf-8-sig',index=False)
+conn = pymysql.connect(host='localhost',user='root',password='bobai123',db='bobai3',charset='utf8mb4')
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+sql = "select * from ai_feature join pair_info on ai_feature.pair_id = pair_info.id order by pair_info.created_at_timestamp desc limit 0,1500" 
+cursor.execute(sql)
+datas = cursor.fetchall()
+for data in tqdm(datas,desc="prcessing"):
+    creator = data['token00_creator']
+    sql1 = "select count(*) from pair_info where token00_creator = %s" 
+    cursor.execute(sql1,creator)
+    number = cursor.fetchall()[0]['count(*)']
+    sql2 = "update ai_feature set number_of_token_creation_of_creator = %s where token_id = %s"
+    cursor.execute(sql2,(number,data['token00_id']))
