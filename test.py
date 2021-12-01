8df20f56e3d32badd2ff4ea3207cbcbff926eacb
Myeongseokkang <kms00129@naver.com>
2021-12-01 18:49:24 +0900

    fetch: ai_score 테스트

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
-import pymysql
+from tensorflow import keras
 import pandas as pd
-from tqdm import tqdm
-conn = pymysql.connect(host='localhost',user='root',password='bobai123',db='bobai3',charset='utf8mb4')
-cursor = conn.cursor(pymysql.cursors.DictCursor)
-sql = "select * from ai_feature join pair_info on ai_feature.pair_id = pair_info.id order by pair_info.created_at_timestamp desc limit 0,1500" 
-cursor.execute(sql)
-datas = cursor.fetchall()
-for data in tqdm(datas,desc="prcessing"):
-    creator = data['token00_creator']
-    sql1 = "select count(*) from pair_info where token00_creator = %s" 
-    cursor.execute(sql1,creator)
-    number = cursor.fetchall()[0]['count(*)']
-    sql2 = "update ai_feature set number_of_token_creation_of_creator = %s where token_id = %s"
-    cursor.execute(sql2,(number,data['token00_id']))
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
-conn.commit()
-conn.close()
+dataset = pd.read_csv('./ai_feature/Dataset_v1.0.csv')
+origin = dataset
+dataset = dataset.drop(columns = ['id', 'lp_avg'])
+dataset = dataset.dropna(how='any',axis = 0)
+
+scaler = MinMaxScaler()
+dataset[ : ] = scaler.fit_transform(dataset[ : ])
+
+model = keras.models.load_model('./ann96.h5')
+model.summary()
+
+result = model.predict(dataset)
+origin['predict'] = result
+
+origin.to_csv('./dataset_AIscore.csv')
