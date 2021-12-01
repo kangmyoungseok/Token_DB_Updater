b0c1074f098c2af0092cb3cb37ebfb375a19d70f
Myeongseokkang <kms00129@naver.com>
2021-12-01 15:29:35 +0900

    fetch: 업데이터 코드 수정

--- updater3.py
+++ updater3.py
@@ -6,6 +6,7 @@ from lib.FeatureLib import *
 from decimal import Decimal
 import time
 from tqdm import tqdm
+import datetime
 
 #1. 기존 파일에 존재하는 가장 최신 토큰이후에 생긴 토큰 DB에 추가
 conn = pymysql.connect(host='localhost', user='root', password='bobai123', db='bobai3', charset='utf8mb4') 
@@ -34,9 +35,6 @@ if(len(datas) == 1000):
         switch_token(result)
         datas3 = pd.json_normalize(result['data']['pairs']).to_dict('records')
         datas.extend(datas3)
-
-
-
 
 
 
@@ -120,7 +118,7 @@ for data in tqdm(datas,desc="get features: "):
         
         created_at_timestamp = data['createdAtTimestamp']
         cursor.execute(sql2,data['token00_creator'])
-        number_of_token_creation_of_creator = cursor.fetchone()[0] + 1
+        number_of_token_creation_of_creator = cursor.fetchone()[0] 
                
         cursor.execute(sql,(token_id,pair_id,mint_count,swap_count,burn_count,active_period,mint_mean_period,swap_mean_period,burn_mean_period,swap_in,swap_out,lp_lock_ratio,lp_avg,lp_std,lp_creator_holding_ratio,burn_ratio,token_creator_holding_ratio,created_at_timestamp,number_of_token_creation_of_creator))
     except Exception as e:
@@ -170,32 +168,24 @@ UPDATE pair_info set is_scam=true where token00_id = %s
 
 
 for data in tqdm(datas,desc="rugpull check: ") :
-  if( (data['is_scam'] == 1) or (data['tx_count'] < 3)  )  :
-    continue
-  try:
-    before_ETH = Decimal(data['reserve_ETH'])
-    current_ETH = Decimal(sus_list[data['id']])
-    
-    if( (current_ETH / before_ETH) < 0.01 ): # 이 조건을 만족한다면, 러그풀이 발생했을 거라 판단하고, 러그풀 발생지점을 찾는 코드를 돌린다.
-      
-      is_rugpull_occur(data)
-      if(data['is_scam'] == True):  #DB에 데이터 넣기
-        print("러그풀 발생 before : %d , after : %d",before_ETH,current_ETH)
-        token_id = data['token_id']
-        pair_id = data['id']
-        tx_id = data['tx_id']
-        rugpull_timestamp = data['rugpull_timestamp']
-        before_ETH = data['before_ETH']
-        after_ETH = data['after_ETH']
-        cursor.execute(sql2,token_id)
-        cursor.execute(sql,(token_id,pair_id,tx_id,rugpull_timestamp,before_ETH,after_ETH))
-      else:
+    if( (data['is_scam'] == 1) or (data['tx_count'] < 3)  )  :
         continue
-      
-
-  except Exception as e:
-    print(e)
-    continue
+    try:
+        if(data['reserve_ETH'] < 0.01):
+            is_rugpull_occur(data)
+            if(data['is_scam'] == True):  #DB에 데이터 넣기
+                token_id = data['token_id']
+                pair_id = data['id']
+                tx_id = data['tx_id']
+                rugpull_timestamp = data['rugpull_timestamp']
+                before_ETH = data['before_ETH']
+                after_ETH = data['after_ETH']
+                print("러그풀 발생 before : %d , after : %d",before_ETH,after_ETH)
+                cursor.execute(sql2,token_id)
+                cursor.execute(sql,(token_id,pair_id,tx_id,rugpull_timestamp,before_ETH,after_ETH))
+    except Exception as e:
+        print(e)
+        continue
 
 conn.commit()
 
@@ -259,7 +249,7 @@ mint_mean_period = %s, swap_mean_period = %s, burn_mean_period=%s, swap_in = %s,
 token_id = %s
 '''
 
-for data in tqdm(datas,desc="update token feature: "):
+for data in tqdm(datas,desc="update token feature:"):
     if( (data['is_change'] == True )and (data['is_scam'] == False) ):
         try:
             data['token0.name'] = data['token0_name']
@@ -301,3 +291,50 @@ for data in tqdm(datas,desc="update token feature: "):
 
 conn.commit()
 conn.close()
+
+
+#6. Feature들의 업데이트 이후, 업데이트된 Feature들로 AI 모델에 넣어야 한다.
+# 스캠이 발생한 토큰[is_scam == true]은 ai 모델에 넣지 않는다.
+# datas에 있는 토큰들에 대해서 진행
+conn = pymysql.connect(host='localhost',user='root',password='bobai123',db='bobai3',charset='utf8mb4')
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+sql = "select * from ai_feature join pair_info on ai_feature.pair_id = pair_info.id where pair_info.created_at_timestamp > %d " %timestamp
+cursor.execute(sql)
+datas = cursor.fetchall()
+
+result = []
+for data in datas:
+    if(data['is_scam'] == 1):
+        continue
+    dataset = {}
+    try:
+        dataset['id'] = data['pair_id'] 
+        dataset['mint_count_per_week'] = data['mint_count'] / ((int(data['active_period']) / (60* 60 * 24 * 7)) + 1)
+        dataset['burn_count_per_week'] =data['burn_count'] / ((int(data['active_period']) / (60* 60 * 24 * 7)) + 1)
+        tx_count = data['mint_count'] + data['burn_count'] + data['swap_count']
+        dataset['mint_ratio'] = data['mint_count'] / tx_count
+        dataset['swap_ratio'] = data['swap_count'] / tx_count
+        dataset['burn_ratio'] = data['burn_count'] / tx_count
+        dataset['mint_mean_period'] = data['mint_mean_period'] / data['active_period']
+        dataset['swap_mean_period'] = data['swap_mean_period'] / data['active_period']
+        dataset['burn_mean_period'] = data['burn_mean_period'] / data['active_period']
+        dataset['swap_in_per_week'] = data['swap_in'] /((int(data['active_period']) / (60* 60 * 24 * 7)) + 1)
+        dataset['swap_out_per_week'] = data['swap_out'] / ((int(data['active_period']) / (60* 60 * 24 * 7)) + 1)
+        dataset['swap_rate'] = data['swap_in'] / (data['swap_out'] + 1)
+        dataset['lp_avg'] = data['lp_avg'] 
+        dataset['lp_std'] = data['lp_std']
+        dataset['lp_creator_holding_ratio'] = data['lp_creator_holding_ratio']
+        dataset['lp_lock_ratio'] = data['lp_lock_ratio']
+        dataset['token_burn_ratio'] = data['burn_ratio']
+        dataset['token_creator_holding_ratio'] = data['token_creator_holding_ratio']
+        dataset['number_of_token_creation_of_creator'] = data['number_of_token_creation_of_creator']
+    except Exception as e:
+        print(e)
+        print([tx_count,data['swap_count'],data['active_period']])
+        continue
+    result.append(dataset)
+
+filename = './ai_feature/Dataset_'+datetime.datetime.now().strftime('%m.%d_%H')+'.csv'
+pd.DataFrame(result).to_csv(filename,encoding='utf-8-sig',index=False)
+    
