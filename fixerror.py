a14ce1886d9db88baefac3724fdd5d8c47c76371
Myeongseokkang <kms00129@naver.com>
2021-12-04 01:24:34 +0900

    fetch: 에러 교정용 파일

---
+++ fixerror.py
@@ -0,0 +1,19 @@
+#Error 수정용 파일
+#DB에 Creator Address가 안들어 있는 애들이 있음.
+
+import pymysql
+import pandas as pd
+from lib.FeatureLib import *
+
+conn = pymysql.connect(host='localhost', user='root', password='bobai123', db='bobai3', charset='utf8mb4') 
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+sql = "select * from pair_info where token00_creator = 'None'"
+sql2 = "Update pair_info set token00_creator = '{}' where token00_id = '{}'"
+cursor.execute(sql)
+datas = cursor.fetchall()
+print(len(datas))
+for data in datas:
+    creator_address = get_creatorAddress(data['id'],data['token00_id'])
+    cursor.execute(sql2,(creator_address,data['token00_id']))
+
+conn.commit()
