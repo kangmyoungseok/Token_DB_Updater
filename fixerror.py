51945c9836983a6164459a4ee89edfc47e56f1c9
Myeongseokkang <kms00129@naver.com>
2021-12-04 01:26:57 +0900

    fix: 문법 오류 수정

--- fixerror.py
+++ fixerror.py
@@ -8,12 +8,13 @@ from lib.FeatureLib import *
 conn = pymysql.connect(host='localhost', user='root', password='bobai123', db='bobai3', charset='utf8mb4') 
 cursor = conn.cursor(pymysql.cursors.DictCursor)
 sql = "select * from pair_info where token00_creator = 'None'"
-sql2 = "Update pair_info set token00_creator = '{}' where token00_id = '{}'"
+sql2 = "Update pair_info set token00_creator = '%s' where token00_id = '%s'"
 cursor.execute(sql)
 datas = cursor.fetchall()
 print(len(datas))
 for data in datas:
     creator_address = get_creatorAddress(data['id'],data['token00_id'])
+    print(creator_address)
     cursor.execute(sql2,(creator_address,data['token00_id']))
 
 conn.commit()
