b89d3d221a896f25ba304d830eb75cfa0a00efa3
Myeongseokkang <kms00129@naver.com>
2021-12-14 17:33:50 +0900

    fetch: pair_info current_score 열 추가 및 업데이터 추가

--- alter_table7.py
+++ alter_table7.py
@@ -5,28 +5,16 @@ from tqdm import tqdm
 conn = pymysql.connect(host='localhost', user='root', password='bobai123', db='bobai3', charset='utf8mb4') 
 cursor = conn.cursor(pymysql.cursors.DictCursor)
 
-sql = "alter table pair_info add column current_score tinyint"
+sql = "update pair_info set current_score = -1"
 cursor.execute(sql)
 
-sql2 = "select * from pair_info where is_scam = 0 "
-cursor.execute(sql2)
-datas = cursor.fetchall()
 
-sql3 = "select * from pair_info join graph_table on pair_info.id = graph_table.pair_id where is_scam=0"
-cursor.execute(sql3)
+sql1 = "select id,graph_table.current_score from pair_info join graph_table on pair_info.id = graph_table.pair_id where is_scam=0"
+cursor.execute(sql1)
 datas2 = cursor.fetchall()
 
-score_list = []
+sql2 ="update pair_info set current_score = %s where id = %s"
 for data in datas2:
-    score_list.append( { data['id'] : data['graph_table.current_score']  } )
+    cursor.execute(sql2,(data['current_score'],data['id']))
 
 
-sql4 = "update pair_info set current_score = %s where id = %s"
-for data in datas:
-    try:
-        current_score = score_list[data['id']]
-    except:
-        current_score = -1
-    finally:
-        cursor.execute(sql4,(current_score,data['id']))
-        conn.commit()
--- updater3.py
+++ updater3.py
@@ -516,25 +516,15 @@ for data in datas:
 
 
 # newest current_score
-sql2 = "select * from pair_info where is_scam = 0 "
-cursor.execute(sql2)
-datas = cursor.fetchall()
+sql = "update pair_info set current_score = -1"
+cursor.execute(sql)
 
-sql3 = "select * from pair_info join graph_table on pair_info.id = graph_table.pair_id where is_scam=0"
-cursor.execute(sql3)
+
+sql1 = "select id,graph_table.current_score from pair_info join graph_table on pair_info.id = graph_table.pair_id where is_scam=0"
+cursor.execute(sql1)
 datas2 = cursor.fetchall()
 
-score_list = []
+sql2 ="update pair_info set current_score = %s where id = %s"
 for data in datas2:
-    score_list.append( { data['id'] : data['graph_table.current_score']  } )
-
-
-sql4 = "update pair_info set current_score = %s where id = %s"
-for data in datas:
-    try:
-        current_score = score_list[data['id']]
-    except:
-        current_score = -1
-    finally:
-        cursor.execute(sql4,(current_score,data['id']))
-        conn.commit()
+    cursor.execute(sql2,(data['current_score'],data['id']))
+    conn.commit()
